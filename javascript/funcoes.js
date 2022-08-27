6136fc17d13e4ac0d91db48ff7fb133a9c160a1f
Vinicius <viniciusit.moreira@gmail.com>
2022-08-26 21:01:21 -0300

    commit

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,3 +1,12 @@
 function mudaFoto() {
 	document.querySelector(".cabecalho").opacity = (.5);
 }
+
+const scrolltotop = () => {
+	window.scroll ({
+		top: 0,
+		behavior: 'smooth',
+	});
+};
+
+document.querySelector('scroll').onclick = scrolltotop;
